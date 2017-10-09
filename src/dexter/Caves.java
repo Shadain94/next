22f8d1efa49164f3ba584b835ff866f5b0955b1a
Shadain94 <shadain.gooden@mymona.uwi.edu>
2017-10-08 21:11:42 -0500

    i made a change

--- src/dexter/Caves.java
+++ src/dexter/Caves.java
@@ -15,7 +15,14 @@ public class Caves {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hey work");
-
+		int i= 5;
+		while(i>0){
+			
+			System.out.println(" the value of i is "+ i);
+			i--;
+		}
+		
+		System.out.println("Finally the value of i is "+ i);
 	}
 
 }
